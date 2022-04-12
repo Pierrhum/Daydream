0bc0e39a1635e851049a132ffb00016ca79c59f9
Pierrhum <34352456+Pierrhum@users.noreply.github.com>
2022-04-12 23:28:48 +0200

    CardFight WIP - UI Transition & Music

--- DayDreaming/Assets/Script/Menu/CardsFight.cs
+++ DayDreaming/Assets/Script/Menu/CardsFight.cs
@@ -2,36 +2,53 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using Coffee.UIEffects;
 
 public class CardsFight : MonoBehaviour
 {
+    public Music music;
     public Image Overlay;
+    public Image PlayerSprite;
+    public Image EnemySprite;
     public GameObject PlayerHand;
+
+    private List<Image> fadeImages = new List<Image>();
+
+    private UIDissolve dissolve;
 
     public void Start()
     {
+        dissolve = Overlay.GetComponent<UIDissolve>();
+        dissolve.effectFactor = 1.0f;
+        // Fill the image list to fade
+        fadeImages.Add(PlayerSprite);
+        fadeImages.Add(EnemySprite);
+        foreach (Transform child in PlayerHand.transform)
+            fadeImages.Add(child.GetComponent<Image>());
+
         SetOpacity(0.0f);
-        Utils.UI.SetImageOpacity(Overlay, 0.0f);
         gameObject.SetActive(false);
+
     }
 
     public void Open()
     {
         gameObject.SetActive(true);
+        GameManager.instance.soundManager.music.Stop(false);
+        music.Play();
         StartCoroutine(ShowCoroutine());
     }
 
     private IEnumerator ShowCoroutine()
     {
-        yield return StartCoroutine(Utils.UI.Fade(Overlay, 0.0f, 1.0f, 0.5f));
-        SetOpacity(1.0f);
+        yield return StartCoroutine(Utils.UI.Dissolve(dissolve, 1.0f, 0.0f, 1.0f));
+        
+        yield return StartCoroutine(Utils.UI.Fade(fadeImages, 0.0f, 1.0f, 0.2f));
     }
 
     private void SetOpacity(float a)
     {
-        foreach (Transform child in PlayerHand.transform)
-        {
-            Utils.UI.SetImageOpacity(child.GetComponent<Image>(), a);
-        }
+        foreach (Image img in fadeImages)
+            Utils.UI.SetImageOpacity(img, a);
     }
 }
--- DayDreaming/Assets/Script/Menu/Utils.cs
+++ DayDreaming/Assets/Script/Menu/Utils.cs
@@ -2,16 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using Coffee.UIEffects;
 
 namespace Utils {
     public static class UI
     {
+        #region Basic Unity Effects
         public static void SetImageOpacity(Image image, float a)
         {
             Color c = image.color;
             image.color = new Color(c.r, c.g, c.b, a);
         }
-        public static IEnumerator Fade(Image img, float start, float end, float duration)
+        public static IEnumerator Fade(List<Image> images, float start, float end, float duration)
         {
             float timer = 0f;
             AnimationCurve smoothCurve = new AnimationCurve(new Keyframe[] { new Keyframe(0f, 0f), new Keyframe(1f, 1f) });
@@ -19,11 +21,32 @@ namespace Utils {
             while (timer <= duration)
             {
                 timer += Time.deltaTime;
-                Color c = img.color;
-                img.color = new Color(c.r, c.g, c.b, Mathf.Lerp(start, end, smoothCurve.Evaluate(timer / duration)));
+                foreach(Image img in images)
+                {
+                    Color c = img.color;
+                    img.color = new Color(c.r, c.g, c.b, Mathf.Lerp(start, end, smoothCurve.Evaluate(timer / duration)));
+                }
                 yield return new WaitForSeconds(Time.deltaTime);
             }
         }
+        #endregion
+
+        #region UiEffect Tool
+
+        public static IEnumerator Dissolve(UIDissolve dissolve, float start, float end, float duration)
+        {
+            float timer = 0f;
+            AnimationCurve smoothCurve = new AnimationCurve(new Keyframe[] { new Keyframe(0f, 0f), new Keyframe(1f, 1f) });
+
+            while (timer <= duration)
+            {
+                timer += Time.deltaTime;
+                dissolve.effectFactor = Mathf.Lerp(start, end, smoothCurve.Evaluate(timer / duration));
+                yield return new WaitForSeconds(Time.deltaTime);
+            }
+        }
+
+        #endregion
     }
 };
 
