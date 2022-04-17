d4b6e21d07c883f06141d87ce6f8a7412b67abe5
Pierrhum <34352456+Pierrhum@users.noreply.github.com>
2022-04-17 11:04:48 +0200

    Cinematic system done

--- DayDreaming/Assets/Script/Player/Quest.cs
+++ DayDreaming/Assets/Script/Player/Quest.cs
@@ -11,27 +11,10 @@ public class Quest : ScriptableObject
 
     public QuestGiver Trigger;
 
-    public List<Dialogue> Dialogues;
-
-    public void End()
-    {
-        // Peut être associer une cinématique ou un dialogue ici
-        Debug.Log(Name + " ended");
-    }
+    public Cinematic Cinematic;
 
     public Vector3 GetTriggerPos()
     {
         return Trigger.gameObject.transform.position;
-    }
-}
-
-[System.Serializable]
-public class Dialogue : Object
-{
-    public string Text;
-
-    public Dialogue(string Text)
-    {
-        this.Text = Text;
     }
 }
