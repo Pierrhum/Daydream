095389018004838c0fabaeccc577a1f10d3a1a65
Pierrhum <34352456+Pierrhum@users.noreply.github.com>
2022-04-17 09:36:41 +0200

    Kinematic system wip

--- DayDreaming/Assets/Script/Player/Quest.cs
+++ DayDreaming/Assets/Script/Player/Quest.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[CreateAssetMenu(fileName = "New Quest", menuName = "Quest")]
+[CreateAssetMenu(fileName = "New Quest", menuName = "QuestSystem/Quest")]
 public class Quest : ScriptableObject
 {
     public string Name;
@@ -10,6 +10,8 @@ public class Quest : ScriptableObject
     public Quest Next;
 
     public QuestGiver Trigger;
+
+    public List<Dialogue> Dialogues;
 
     public void End()
     {
@@ -20,5 +22,16 @@ public class Quest : ScriptableObject
     public Vector3 GetTriggerPos()
     {
         return Trigger.gameObject.transform.position;
+    }
+}
+
+[System.Serializable]
+public class Dialogue : Object
+{
+    public string Text;
+
+    public Dialogue(string Text)
+    {
+        this.Text = Text;
     }
 }
